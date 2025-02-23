33a516690aab9eed52e69e1d7536add1b2ac8f42
sermarmar <sermarmar1995@gmail.com>
2025-02-23 12:23:14 +0100

    Menú completed responsive

--- src/components/commons/Button.tsx
+++ src/components/commons/Button.tsx
@@ -1,12 +1,20 @@
 import type React from "react";
 
 interface ButtonProps {
-    text: string;
+    children: React.ReactNode;
+    color?: string;
+    txColor?: string;
     onClick?: () => void;
 }
 
-export const Button: React.FC<ButtonProps> = ({text, onClick}) => {
+export const Button: React.FC<ButtonProps> = ({children, color, txColor, onClick}) => {
+    const bgColor = color ? `bg-${color}` : "bg-terracotta-600";
+    let textColor = txColor ? `text-${txColor}` : "text-white";
+    if(color && color === 'transparent') {
+        textColor = 'text-gray-900';
+    }
+
     return (
-        <button className="px-4 py-2 rounded-sm bg-terracotta-600 text-white transition hover:bg-terracotta-800" onClick={onClick}>{ text }</button>
+        <button className={`px-4 py-2 rounded-sm ${bgColor} ${textColor} transition hover:bg-terracotta-800 hover:text-gray-50`} onClick={onClick}>{ children }</button>
     )
 }
